c18ac7ff5af66245360add0ce28350ea5fcf05f4
Rashi <rashidulhuda44@gmail.com>
2026-04-16 07:15:31 +0530

    chapter 4 image assets added

--- map-chapter.js
+++ map-chapter.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const path = require("path");
 
 // --- CONFIGURATION ---
-const CHAPTER_INDEX = 3; 
+const CHAPTER_INDEX = 4; 
 // ---------------------
 
 const TARGET_DIR = path.join("assets", `chapter${CHAPTER_INDEX}`);
